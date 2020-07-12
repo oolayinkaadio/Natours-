1d54d4757e234c09b765a4aa692abf84dc107b23
Olaosebikan <oolayinkaadio@gmail.com>
2020-07-12 17:22:20 +0100

    Fixed a bug where the sharp modules was used

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -40,11 +40,11 @@ exports.resizeUserPhoto = catchAsync(async(req, res, next) => {
 
     req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-    await sharp(req.file.photo.buffer)
+    await sharp(req.file.buffer)
         .resize(500, 500)
         .toFormat('jpg')
         .jpeg({
-            quality: 90,
+            quality: 90
         })
         .toFile(`public/img/users/${req.file.filename}`);
 
